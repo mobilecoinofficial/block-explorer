83f28a97d68141d81383bec904c34c9fb4ca654c
Colin Carey <colin.carey1@gmail.com>
2023-01-23 10:27:30 -0800

    improve block row styles

--- src/components/BlockRow.tsx
+++ src/components/BlockRow.tsx
@@ -1,4 +1,8 @@
 import { Link } from "react-router-dom";
+import { css } from "@emotion/react";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import moment from "moment";
 
 import { Block } from "api/types";
 import { abbreviateHash } from "utils/truncate";
@@ -7,25 +11,43 @@ type BlockRowProps = {
     block: Block;
 };
 
+const styles = {
+    // TODO constants for all these colors
+    link: css`
+        color: #666666;
+        text-decoration: none;
+    `
+};
+
 export function getTimeStamp(block: Block): string {
     if (block.timestampResultCode !== "TimestampFound") {
         return "Timestamp not available";
     }
 
-    return new Date(parseInt(block.timestamp) * 1000).toUTCString();
+    return moment(parseInt(block.timestamp) * 1000).format("MMM D YYYY, h:mm:ss A");
 }
 
 export default function BlockRow({ block }: BlockRowProps) {
+    function copyHash() {
+        navigator.clipboard.writeText(block.contentsHash);
+    }
+
     return (
         <tr>
             <td>{block.index}</td>
-            <td>{abbreviateHash(block.contentsHash)}</td>
+            <td style={{ display: "flex", alignItems: "center", cursor: "pointer" }}>
+                <span style={{ width: 108 }}>{abbreviateHash(block.contentsHash)}</span>
+                <ContentCopyIcon css={styles.link} fontSize="small" onClick={copyHash} />
+            </td>
             <td>{block.outputs.length}</td>
             <td>{block.keyImages.length}</td>
             <td>{block.signatures?.length}</td>
             <td>{getTimeStamp(block)}</td>
-            <td style={{ cursor: "pointer" }}>
-                <Link to={block.index}>details</Link>
+            <td style={{ cursor: "pointer", display: "flex", alignItems: "center" }}>
+                <Link css={styles.link} to={block.index}>
+                    details
+                </Link>
+                <NavigateNextIcon css={styles.link} fontSize="small" />
             </td>
         </tr>
     );
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,10 @@
 import { createTheme } from "@mui/material/styles";
+import { purple, teal } from "@mui/material/colors";
 
 const theme = createTheme({
     palette: {
+        primary: purple,
+        secondary: teal,
         background: {
             default: "#faf8f6"
         }
