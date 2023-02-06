591be288a5e538ec97496474efef12ae1141ceec
Colin Carey <colin.carey1@gmail.com>
2023-02-06 10:55:32 -0800

    Improve no block found page

--- src/pages/NoBlockFound.tsx
+++ src/pages/NoBlockFound.tsx
@@ -1,8 +1,24 @@
 import { useParams } from "react-router-dom";
+import { Box, Typography, Link } from "@mui/material";
 
 import Page from "components/Page";
 
 export default function NoBlockFound() {
     const { blockIndex } = useParams();
-    return <Page>No block with index {blockIndex} found.</Page>;
+    return (
+        <Page>
+            <Box
+                width="100%"
+                height="100%"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                flexDirection="column"
+                minHeight={400}
+            >
+                <Typography variant="h5"> No block with index {blockIndex} found.</Typography>
+                <Link href="/blocks">return to recent blocks</Link>
+            </Box>
+        </Page>
+    );
 }
