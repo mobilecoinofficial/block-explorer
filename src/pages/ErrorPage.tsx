b51f38d660f5ad0d82e7c2bf6675f5e0de86c67f
Colin Carey <colin.carey1@gmail.com>
2023-02-10 16:18:21 -0800

    Improve error apge

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,7 @@
-import { Box, Typography, Link, Container } from "@mui/material";
+import { AppBar, Box, Typography, Toolbar, Container, Link } from "@mui/material";
 import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+
+import MobileCoinLogo from "components/MobileCoinLogo";
 
 export default function ErrorPage() {
     const error = useRouteError();
@@ -18,8 +20,29 @@ export default function ErrorPage() {
 }
 
 function ErrorWrapper({ title, errorText }: { title: string; errorText?: string }) {
+    console.error(errorText);
     return (
         <Container>
+            <AppBar>
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <Box display="flex" alignItems="center">
+                            <MobileCoinLogo />
+                            <Typography
+                                variant="h5"
+                                noWrap
+                                sx={{
+                                    flexGrow: 1,
+                                    display: { xs: "none", sm: "block" },
+                                    marginLeft: 1
+                                }}
+                            >
+                                MobileCoin Block Explorer
+                            </Typography>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
             <Box
                 width="100%"
                 height="100%"
@@ -33,7 +56,7 @@ function ErrorWrapper({ title, errorText }: { title: string; errorText?: string 
                 <Link href="/blocks" sx={{ marginBottom: 4 }}>
                     return to recent blocks
                 </Link>
-                <Typography color="error">{errorText}</Typography>
+                <Typography color="error">see console for details</Typography>
             </Box>
         </Container>
     );
