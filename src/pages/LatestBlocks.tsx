c324de3d81ed180827a0ddd1aa2bb9921b5cfcc8
Colin Carey <colin.carey1@gmail.com>
2023-02-15 11:43:21 -0800

    improve stickky header

--- src/pages/LatestBlocks.tsx
+++ src/pages/LatestBlocks.tsx
@@ -1,5 +1,5 @@
-import InfiniteScroll from "react-infinite-scroll-component";
-import { useState, useCallback } from "react";
+// import InfiniteScroll from "react-infinite-scroll-component";
+import { useState, useCallback, useRef, useLayoutEffect } from "react";
 import {
     Box,
     Container,
@@ -10,7 +10,8 @@ import {
     TableContainer,
     TableHead,
     TableRow,
-    TableBody
+    TableBody,
+    Collapse
 } from "@mui/material";
 
 import { useSyncData } from "pages/Layout";
@@ -34,11 +35,16 @@ const HeaderNumber = ({ title, value }: HeaderNumberProps) => (
 );
 
 export default function LatestBlocks() {
+    const tableEl = useRef(null);
+    const [renderTopContents, setRenderTopContents] = useState(true);
     const { preLoadedBlocks, networkStatus } = useSyncData();
     const [blocks, setBlocks] = useState<Block[]>(preLoadedBlocks as Block[]);
+    const [distanceBottom, setDistanceBottom] = useState(0);
     const [hasMoreBlocks, setHasMoreBlocks] = useState<boolean>(true);
+    const [loading, setLoading] = useState(false);
 
-    const getMoreBlocks = useCallback(async () => {
+    const loadMoreBlocks = useCallback(async () => {
+        setLoading(true);
         let blockIndex = parseInt(blocks[blocks.length - 1].index) - INITIAL_BLOCK_COUNT;
         let limit = INITIAL_BLOCK_COUNT;
         if (blockIndex < 0) {
@@ -53,57 +59,94 @@ export default function LatestBlocks() {
         if (blocks[blocks.length - 1].index === "0") {
             setHasMoreBlocks(false);
         }
+        setLoading(false);
     }, [blocks]);
 
+    const scrollListener = useCallback(() => {
+        if (tableEl.current.scrollTop === 0 && !renderTopContents) {
+            setRenderTopContents(true);
+        } else if (renderTopContents) {
+            setRenderTopContents(false);
+        }
+
+        // if (tableEl.current.scrollTop != 0 && renderTopContents) {
+        // }
+        const bottom = tableEl.current.scrollHeight - tableEl.current.clientHeight;
+        // if you want to change distanceBottom every time new data is loaded
+        // don't use the if statement
+        if (!distanceBottom) {
+            // calculate distanceBottom that works for you
+            setDistanceBottom(Math.round(bottom * 0.2));
+        }
+        if (tableEl.current.scrollTop > bottom - distanceBottom && hasMoreBlocks && !loading) {
+            loadMoreBlocks();
+        }
+    }, [hasMoreBlocks, loadMoreBlocks, loading, distanceBottom, renderTopContents]);
+
+    useLayoutEffect(() => {
+        const tableRef = tableEl.current;
+        tableRef.addEventListener("scroll", scrollListener);
+        return () => {
+            tableRef.removeEventListener("scroll", scrollListener);
+        };
+    }, [scrollListener]);
+
+    const getTableHeightToSubtract = useCallback(() => {
+        // This is a little brittle a needs to be adjusted if we modify the top of this page.
+        // We want the table to be as tall as possible, but we need to provide a fixed height
+        // in order for the infinite scroll to work
+        const headerHeight = 64;
+        const headerPadding = 16;
+        const topContentsHeight = 116;
+        const tableBottomPadding = 32;
+        let tableHeightToSubtract =
+            headerHeight + headerPadding + topContentsHeight + tableBottomPadding;
+        if (!renderTopContents) {
+            tableHeightToSubtract -= topContentsHeight;
+        }
+        return tableHeightToSubtract;
+    }, [renderTopContents]);
+
     return (
-        <Container>
-            <Grid container sx={{ marginBottom: 2, marginTop: 1 }}>
-                <Grid item xs={6} justifyContent="center" display="flex">
-                    <HeaderNumber
-                        title="Number of Blocks"
-                        value={networkStatus?.localBlockHeight}
-                    />
+        <Container sx={{ overflow: "hidden" }}>
+            {/* <div style={{ display: renderTopContents ? "inherit" : "none" }}> */}
+            <Collapse in={renderTopContents} timeout={600}>
+                <Grid container sx={{ marginBottom: 2, marginTop: 1 }}>
+                    <Grid item xs={6} justifyContent="center" display="flex">
+                        <HeaderNumber
+                            title="Number of Blocks"
+                            value={networkStatus?.localBlockHeight}
+                        />
+                    </Grid>
+                    <Grid item xs={6} justifyContent="center" display="flex">
+                        <HeaderNumber title="Transaction Outputs" value={networkStatus?.numTxos} />
+                    </Grid>
                 </Grid>
-                <Grid item xs={6} justifyContent="center" display="flex">
-                    <HeaderNumber title="Transaction Outputs" value={networkStatus?.numTxos} />
-                </Grid>
-            </Grid>
-            <Typography variant="h4">Latest Blocks</Typography>
-            <div>
-                <InfiniteScroll
-                    dataLength={blocks.length}
-                    next={getMoreBlocks}
-                    hasMore={hasMoreBlocks}
-                    loader={<Typography variant="h4">Loading...</Typography>}
-                    endMessage={
-                        <div style={{ textAlign: "center" }}>
-                            <Typography variant="h4">
-                                Wow! you scrolled through all the blocks. Impressive!
-                            </Typography>
-                        </div>
-                    }
-                >
-                    <TableContainer>
-                        <Table sx={{ borderCollapse: "separate", borderSpacing: "0px 12px" }}>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Index</TableCell>
-                                    <TableCell>Hash</TableCell>
-                                    <TableCell>TXOs</TableCell>
-                                    <TableCell>IMGs</TableCell>
-                                    <TableCell>SIGs</TableCell>
-                                    <TableCell>Timestamp</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {blocks.map((block) => (
-                                    <BlockRow block={block} key={block.id} />
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </InfiniteScroll>
-            </div>
+                <Typography variant="h4">Latest Blocks</Typography>
+            </Collapse>
+            {/* </div> */}
+            <TableContainer
+                sx={{ maxHeight: `calc(100vh - ${getTableHeightToSubtract()}px)` }}
+                ref={tableEl}
+            >
+                <Table sx={{ borderCollapse: "separate", borderSpacing: "0px 12px" }} stickyHeader>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Index</TableCell>
+                            <TableCell>Hash</TableCell>
+                            <TableCell>TXOs</TableCell>
+                            <TableCell>IMGs</TableCell>
+                            <TableCell>SIGs</TableCell>
+                            <TableCell>Timestamp</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {blocks.map((block) => (
+                            <BlockRow block={block} key={block.id} />
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Container>
     );
 }
